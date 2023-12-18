d02b3455651d6ea328c99dfa2f4d4def42168366
KimByungWoo <89980923+BuildEnough@users.noreply.github.com>
2023-12-18 13:09:33 +0900

    초기화

--- springboot-developer/src/main/java/me/enough/Main.java
+++
@@ -1,7 +0,0 @@
-package me.enough;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- springboot-developer/src/main/java/me/enough/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package me.enough.springbootdeveloper.repository;
-
-import me.enough.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
