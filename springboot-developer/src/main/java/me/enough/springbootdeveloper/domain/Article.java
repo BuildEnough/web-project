878fd8075d9ef28e3d661bb39becfd608c41cb44
BuildEnough <rhtmaehcl878@naver.com>
2023-12-18 12:22:08 +0900

    thymeleaf 연습

--- springboot-developer/src/main/java/me/enough/springbootdeveloper/domain/Article.java
+++ springboot-developer/src/main/java/me/enough/springbootdeveloper/domain/Article.java
@@ -1,14 +1,10 @@
 package me.enough.springbootdeveloper.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Getter
